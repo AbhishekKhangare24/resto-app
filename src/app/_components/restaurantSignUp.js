ad3a70202c8c7bcbd890a66ec82d029323081cd6
Abhishek Khangare <abhi@gmail.com>
2024-09-16 19:55:42 +0530

    Added state variables and handlers for form inputs in RestaurantSignUp component

--- src/app/_components/restaurantSignUp.js
+++ src/app/_components/restaurantSignUp.js
@@ -1,15 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 
 const RestaurantSignUp = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [c_password, setC_password] = useState("");
+  const [name, setName] = useState("");
+  const [city, setCity] = useState("");
+  const [address, setAddress] = useState("");
+  const [contact, setContact] = useState("");
+
+  const handleSignup = () => {
+    console.log(email, password, c_password, name, city, address, contact);
+  };
+
   return (
     <>
-      <h3>SignUp Component</h3>
+      <h3>SignUp</h3>
       <div>
         <div className="input-wrapper">
           <input
             type="text"
             placeholder="Enter email id"
             className="input-field"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
           />
         </div>
         <div className="input-wrapper">
@@ -17,6 +31,8 @@ const RestaurantSignUp = () => {
             type="password"
             placeholder="Enter password"
             className="input-field"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
           />
         </div>
         <div className="input-wrapper">
@@ -24,6 +40,8 @@ const RestaurantSignUp = () => {
             type="password"
             placeholder="Confirm password"
             className="input-field"
+            value={c_password}
+            onChange={(e) => setC_password(e.target.value)}
           />
         </div>
         <div className="input-wrapper">
@@ -31,31 +49,41 @@ const RestaurantSignUp = () => {
             type="text"
             placeholder="Enter restaurant name"
             className="input-field"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
           />
         </div>
         <div className="input-wrapper">
           <input
-            type="password"
+            type="text"
             placeholder="Enter city"
             className="input-field"
+            value={city}
+            onChange={(e) => setCity(e.target.value)}
           />
         </div>
         <div className="input-wrapper">
           <input
-            type="password"
+            type="text"
             placeholder="Enter full address"
             className="input-field"
+            value={address}
+            onChange={(e) => setAddress(e.target.value)}
           />
         </div>
         <div className="input-wrapper">
           <input
-            type="password"
+            type="text"
             placeholder="Enter contact No."
             className="input-field"
+            value={contact}
+            onChange={(e) => setContact(e.target.value)}
           />
         </div>
         <div className="input-wrapper">
-          <button className="button">Sign Up</button>
+          <button className="button" onClick={handleSignup}>
+            Sign Up
+          </button>
         </div>
       </div>
     </>
