7ab688597afad2ed6db9ce0dd8fad3e44f66c9b5
Abhishek Khangare <abhi@gmail.com>
2024-09-17 00:07:03 +0530

    Added state variables and handlers for email, password, and error in RestaurantLogin component

--- src/app/_components/restaurantLogin.js
+++ src/app/_components/restaurantLogin.js
@@ -1,26 +1,53 @@
-import React from "react";
+import React, { useState } from "react";
 
 const RestaurantLogin = () => {
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+  const [error, setError] = useState(false);
+
+  const handleLogin = () => {
+    if (!email || !password) {
+      setError(true);
+    } else {
+      setError(false);
+    }
+    console.log(email, password);
+  };
+
   return (
     <>
-      <h3>Login Component</h3>
+      <h3>Login</h3>
       <div>
         <div className="input-wrapper">
           <input
             type="text"
             placeholder="Enter email id"
             className="input-field"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
           />
+          {error && !email && (
+            <span className="input-error">Please enter valid email</span>
+          )}
         </div>
+
         <div className="input-wrapper">
           <input
             type="password"
             placeholder="Enter password"
             className="input-field"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
           />
+          {error && !password && (
+            <span className="input-error">Please enter valid password</span>
+          )}
         </div>
+
         <div className="input-wrapper">
-          <button className="button">Login</button>
+          <button onClick={handleLogin} className="button">
+            Login
+          </button>
         </div>
       </div>
     </>
