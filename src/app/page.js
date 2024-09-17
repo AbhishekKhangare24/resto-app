28896e975e96e8ac8ad40c4c1b744fc1f28ccb34
Abhishek Khangare <abhi@gmail.com>
2024-09-18 03:02:27 +0530

    Added location dropdown

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,32 @@
+"use client";
+
+import { useEffect, useState } from "react";
 import CustomerHeader from "./_components/CustomerHeader";
 import Footer from "./_components/Footer";
 
 export default function Home() {
+  const [locations, setLocations] = useState([]);
+  const [selectedLocation, setSelectedLocation] = useState("");
+  const [showLocation, setShowLocation] = useState(false);
+
+  useEffect(() => {
+    loadLocations();
+  }, []);
+
+  const loadLocations = async () => {
+    let response = await fetch("http://localhost:3000/api/customer/locations");
+    response = await response.json();
+    if (response.success) {
+      setLocations(response.result);
+    }
+  };
+
+  const handleListItem = (item) => {
+    setSelectedLocation(item);
+    setShowLocation(false);
+  };
+
+  console.log(locations);
   return (
     <main>
       <CustomerHeader />
@@ -10,9 +35,18 @@ export default function Home() {
         <div className="input-wrapper">
           <input
             type="text"
+            value={selectedLocation}
+            onClick={() => setShowLocation(true)}
             className="select-input"
             placeholder="Select Place"
           />
+          <ul className="location-list">
+            {showLocation &&
+              locations.map((item) => (
+                <li onClick={() => handleListItem(item)}> {item} </li>
+              ))}
+          </ul>
+
           <input
             type="text"
             className="search-input"
