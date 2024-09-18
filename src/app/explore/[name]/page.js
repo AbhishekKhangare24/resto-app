6ac34a973c39e0334a809905bb31e233debf3d4c
Abhishek Khangare <abhi@gmail.com>
2024-09-18 10:28:43 +0530

    Add restaurant details api

--- src/app/explore/[name]/page.js
+++ src/app/explore/[name]/page.js
@@ -1,9 +1,14 @@
+import CustomerHeader from "@/app/_components/CustomerHeader";
+
 const page = (props) => {
   const name = props.params.name;
 
   return (
-    <div className="restaurant-page-banner">
-      <h1>{decodeURI(name)}</h1>
+    <div>
+      <CustomerHeader />
+      <div className="restaurant-page-banner">
+        <h1>{decodeURI(name)}</h1>
+      </div>
     </div>
   );
 };
