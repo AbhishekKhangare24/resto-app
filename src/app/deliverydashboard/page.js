8e92c6fe61f0f139abea20190819393470d2f504
Abhishek Khangare <abhi@gmail.com>
2024-09-19 04:03:21 +0530

    Added api for delivery partner

--- src/app/deliverydashboard/page.js
+++ src/app/deliverydashboard/page.js
@@ -1,10 +1,27 @@
 "use client";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import DeliveryHeader from "../_components/DeliveryHeader";
 import { useRouter } from "next/navigation";
 
 const Page = () => {
   const router = useRouter();
+
+  const [myOrders, setMyOrders] = useState("");
+  useEffect(() => {
+    getMyOrders();
+  }, []);
+
+  const getMyOrders = async () => {
+    const deliveryData = JSON.parse(localStorage.getItem("delivery"));
+    let response = await fetch(
+      `http://localhost:3000/api/deliverypartners/orders/${deliveryData._id}`
+    );
+    response = await response.json();
+    if (response.success) {
+      setMyOrders(response.result);
+    }
+  };
+
   useEffect(() => {
     const delivery = JSON.parse(localStorage.getItem("delivery"));
     if (!delivery) {
@@ -15,7 +32,28 @@ const Page = () => {
   return (
     <div>
       <DeliveryHeader />
-      <h1>Delivery Dashboard</h1>
+      <h1>My Order List</h1>
+      {myOrders &&
+        myOrders.map((item) => (
+          <div
+            className="restaurant-wrapper"
+            style={{ marginLeft: "auto", marginRight: "auto" }}
+          >
+            <h4>Name: {item.data.name}</h4>
+            <div>Amount: {item.amount}</div>
+            <div>Address: {item.data.address}</div>
+            <div>Status: {item.status}</div>
+            <div>
+              Update Status:
+              <select>
+                <option>Confirm</option>
+                <option>One the way</option>
+                <option>Delivered</option>
+                <option>Failed To delivered</option>
+              </select>
+            </div>
+          </div>
+        ))}
     </div>
   );
 };
